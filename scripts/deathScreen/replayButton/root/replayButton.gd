5d681ef9a38f2169d377848e89ceba649d762962
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2024-08-13 00:19:54 -0300

    menus finalizados

--- scripts/deathScreen/replayButton/root/replayButton.gd
+++ scripts/deathScreen/replayButton/root/replayButton.gd
@@ -2,4 +2,4 @@ extends Button
 class_name replayButton
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://scenes/level1/root/level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/startScreen/root/startScreen.tscn")
